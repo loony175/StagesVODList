6014b9679a5e5fd14434aec004a4a2090b377beb
loony175 <l1997y03w01@gmail.com>
2018-07-13 03:40:45 +0800

    Updated at 2018-07-13T03:40:45Z (UTC+8)

--- README.md
+++ README.md
@@ -2,19 +2,16 @@ SNH48 Group所有能获取到的公演录播
 
 ## Requirement
 ```
-sudo apt update && sudo apt install -y parallel
-parallel --bibtex
+pip install -U -r requirements.txt
 ```
 
 ## Usage & Updating manually
 ```
-usage: ./bash/stage48 [-j JOBS]
+usage: ./stage48.py [-j JOBS]
 
 optional arguments:
-  -j JOBS, --job JOBS  向*.???48.com发送GET请求时的并发数（并发
-                       数越大，请求效率越高，默认值为CPU逻辑核心
-                       总数的4倍）
+  -j JOBS, --jobs JOBS  向*.???48.com发送GET请求时的并发执行数（并发执行数越大，请求效率越高，默认值为32）
 ```
-工作原理：执行`./bash/stage48`后，首先向*.???48.com发送GET请求以获取原始HTML代码，过滤出单场公演录播的实际URL，并写入normal文件夹下的对应文件中。
+工作原理：执行`./stage48.py`后，首先向*.???48.com发送GET请求以获取原始HTML代码，过滤出单场公演录播的实际URL，并写入normal文件夹下的对应文件中。
 
 本repository中的录播列表我会不定期更新，如需自行手动更新，也可通过执行上述脚本实现。
---
+++ stage48.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import argparse
+import functools
+import multiprocessing
+import pathlib
+import re
+import requests
+
+def get_review_url(group,id):
+    real_group_name={'snh48':'SNH48','bej48':'BEJ48','gnz48':'GNZ48','shy48':'SHY48','ckg48':'CKG48'}
+    path='/Index/invedio/id/%d'%id
+    if group=='snh48':
+        url='http://zhibo.ckg48.com%s'%path
+    else:
+        url='http://live.%s.com%s'%(group,path)
+    while True:
+        resp=requests.get(url)
+        if re.match('^<Response\s\[(?P<code>.*)\]>$',str(resp)).group('code')=='200':
+            if len(resp.text)>16:
+                try:
+                    review_url=re.findall('https?://.*\.m3u8[^"]*',resp.text)[0]
+                except IndexError:
+                    review_url=''
+                break
+            else:
+                print('[%s] %d: Empty response'%(real_group_name[group],id))
+        else:
+            print('[%s] %d: 502 Bad Gateway'%(real_group_name[group],id))
+    return '%d: %s'%(id,review_url)
+
+def main():
+    parser=argparse.ArgumentParser()
+    add=parser.add_argument
+    add('-j','--jobs',type=int,default=32)
+    args=parser.parse_args()
+    file={'snh48':'0001-SNH48.txt','bej48':'2001-BEJ48.txt','gnz48':'3001-GNZ48.txt','shy48':'6001-SHY48.txt','ckg48':'8001-CKG48.txt'}
+    real_group_name={'snh48':'SNH48','bej48':'BEJ48','gnz48':'GNZ48','shy48':'SHY48','ckg48':'CKG48'}
+    dir=pathlib.Path('normal')
+    try:
+        dir.mkdir()
+    except FileExistsError:
+        pass
+    for group_name in ['snh48','bej48','gnz48','shy48','ckg48']:
+        if group_name=='snh48':
+            url='http://zhibo.ckg48.com'
+        else:
+            url='http://live.%s.com'%group_name
+        while True:
+            resp=requests.get(url)
+            if re.match('^<Response\s\[(?P<code>.*)\]>$',str(resp)).group('code')=='200':
+                if len(resp.text)>16:
+                    end_id=max([int(id) for id in re.findall('/Index/invedio/id/(?P<ids>\d+)',resp.text)])
+                    break
+                else:
+                    print('[%s] Index: Empty response'%real_group_name[group_name])
+            else:
+                print('[%s] Index: 502 Bad Gateway'%real_group_name[group_name])
+        work=functools.partial(get_review_url,group_name)
+        pool=multiprocessing.Pool(args.jobs)
+        results=pool.map(work,[id for id in range(1,end_id+1)])
+        pool.close()
+        pool.join()
+        output=dir/file[group_name]
+        f=open(output,'w')
+        for line in results:
+            f.write('%s\n'%line)
+        f.close()
+        print('[%s] %d URLs written in %s'%(real_group_name[group_name],end_id,output))
+
+if __name__=='__main__':
+    main()
